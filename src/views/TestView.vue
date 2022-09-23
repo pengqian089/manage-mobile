6966bcaa91e93ea8d4e479c4930e3d422caa1ce5
pengqian089 <pengqian089@gmail.com>
2022-09-23 17:34:23 +0800

    init

---
+++ src/views/TestView.vue
@@ -0,0 +1,20 @@
+<template>
+  <van-button type="primary" @click="clickButton">button</van-button>
+</template>
+
+<script>
+import { showDialog } from 'vant';
+import 'vant/es/dialog/style';
+export default {
+  name: "TestView",
+  methods:{
+    clickButton(){
+      showDialog({ message: '提示' });
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
